12e5147f3552d5e8eb4ea0290d0ec26c11aacdba
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-10-14 21:16:39 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,16 +34,16 @@ class PostsController < ApplicationController
 
   def edit
     if params[:author_id]
-    author = Author.find_by(id: params[:author_id])
-    if author.nil?
-      redirect_to authors_path, alert: "Author not found."
+      author = Author.find_by(id: params[:author_id])
+      if author.nil?
+        redirect_to authors_path, alert: "Author not found."
+      else
+        @post = author.posts.find_by(id: params[:id])
+        redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
+      end
     else
-      @post = author.posts.find_by(id: params[:id])
-      redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
+      @post = Post.find(params[:id])
     end
-  else
-    @post = Post.find(params[:id])
-  end
   end
 
   private
